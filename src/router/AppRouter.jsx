ab05c270d054af8bd0c14ff1c139c4f3665efe75
Sebastian Gutierrez <u20231212627@usco.edu.co>
2025-10-07 16:56:12 -0500

    Solución de irregularidades

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,16 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
 import Home from "../pages/Home/Home";
-import Home from "../pages/Home/Home";
-import Register from "../pages/Register";
-import Login from "../pages/Login";
+
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Home />} />
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
