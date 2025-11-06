2b0277ac241ead83bab62533bf55baac42eb09d1
Sebastian Gutierrez <u20231212627@usco.edu.co>
2025-11-05 21:54:05 -0500

    Creación sección de marcas populares

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../../components/Home/HeroSection/HeroSection"
+import BrandSection from "../../components/Home/BrandSection/BrandSection";
 
 const Home = () => {
   return (
     <div className="home-page">
       <HeroSection />
+      <BrandSection />
     </div>
   );
 };
