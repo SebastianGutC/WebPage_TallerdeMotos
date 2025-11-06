a7df257dcc1cd5f48061fc040b30369e8a7a4ae3
Sebastian Gutierrez <u20231212627@usco.edu.co>
2025-11-06 00:11:41 -0500

    Creación card de aliados

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../../components/Home/HeroSection/HeroSection"
 import BrandSection from "../../components/Home/BrandSection/BrandSection";
+import AllySection from "../../components/Home/AllySection/AllySection";
 
 const Home = () => {
   return (
     <div className="home-page">
       <HeroSection />
       <BrandSection />
+      <AllySection />
     </div>
   );
 };
