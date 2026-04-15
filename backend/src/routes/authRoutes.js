4b73c817574da4cc11c974192656a601f7a45bc6
Oscar Florez <oscarflorez@usco.edu.co>
2026-04-15 16:28:37 -0500

    Se añadió la funcionalidad de login y logout con cookies

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { registerUsuario} from '../controllers/authController.js';
+import { registerUsuario, loginUsuario, logoutUsuario} from '../controllers/authController.js';
 
 const router = express.Router();
 
 // Rutas públicas
 router.post('/usuarios/register', registerUsuario);
+router.post('/usuarios/login', loginUsuario);
+router.post('/usuarios/logout', logoutUsuario);
 
 export default router;
